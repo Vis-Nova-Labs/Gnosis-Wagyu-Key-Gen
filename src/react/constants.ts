21518e2dfe0a0019f84188d3694dabaabeb1a384
Voss <voss@visnovalabs.io>
2023-08-10 18:32:34 -0500

    update tooltips

--- src/react/constants.ts
+++ src/react/constants.ts
@@ -4,8 +4,8 @@ export const errors = {
 	MNEMONIC_FORMAT: "Invalid format. Your Secret Recovery Phrase should be a 24 word list.",
 	MNEMONICS_DONT_MATCH: "The Secret Recovery Phrase you entered does not match what was given to you. Please try again.",
 	NUMBER_OF_KEYS: "Please input a number between 1 and 1000.",
-	ADDRESS_FORMAT_ERROR: "Please enter a valid Gnosis Chain address.",
-	WITHDRAW_ADDRESS_REQUIRED: "Please enter a Gnosis Chain address.",
+	ADDRESS_FORMAT_ERROR: "Please enter a valid Gnosis Chain/Ethereum address.",
+	WITHDRAW_ADDRESS_REQUIRED: "Please enter a Gnosis Chain/Ethereum address.",
 	PASSWORD_STRENGTH: "Password must be at least 8 characters.",
 	PASSWORD_MATCH: "Passwords don't match.",
 	STARTING_INDEX: "Please input start index.",
@@ -23,12 +23,12 @@ export const errors = {
 export const MNEMONIC_LENGTH = 24;
 
 export const tooltips = {
-	IMPORT_MNEMONIC: "If you've already created a Secret Recovery Phrase, you can use it to regenerate your original keys, create more keys, or generate a BLS to execution change by importing the phrase here.",
+	IMPORT_MNEMONIC: "If you've already created a Secret Recovery Phrase, you can use it to regenerate your original keys, create more keys, or generate a BLS to execution change (BTEC) message by importing the phrase here.",
 	NUMBER_OF_KEYS: "Enter how many new validator keys you'd like to create.",
 	PASSWORD: "Pick a strong password (at least 8 characters) that will be used to protect your keys.",
 	STARTING_INDEX: "Each key is created sequentially, so we need to know how many you've created with this Secret Recovery Phrase in the past in order to create some new ones for you.",
-	ETH1_WITHDRAW_ADDRESS: "A highly reccomended Gnosis Chain address to be used for the withdrawal credentials.",
-	BTEC_WITHDRAW_ADDRESS: "A Gnosis Chain address for withdrawal. There is where your validator balance and rewards will go.",
+	ETH1_WITHDRAW_ADDRESS: "A Gnosis Chain/Ethereum address to be used for the withdrawal credentials.",
+	BTEC_WITHDRAW_ADDRESS: "A Gnosis Chain/Ethereum address for withdrawal. There is where your validator balance and rewards will go.",
 	OFFLINE: "You want to avoid exposing your Secret Recovery Phrase to any system that can send it online and compromise your security. Booting from a live OS that does not connect to any network on a USB drive is an easy way to achieve that. You can copy the resulting files on USB drives. You might want to avoid storing your Secret Recovery Phrase electronically.",
 	BTEC_START_INDEX: "The index position for the keys to start generating withdrawal credentials. If you only created 1 validator key using this Secret Recovery Phrase, this is likely going to be 0. If you created many validator keys, this could be a higher value from where you want to start in the list of validators derived from your Secret Recovery Phrase.",
 	BTEC_INDICES: "A list of the chosen validator index number(s) as identified on the Gnosis Beacon Chain. You can find your validator indice on the gnosischa.in website on your validator page. It will be at the top of that page the form of a title like Validator XXXXX, where XXXXX is going to be your indice.",
